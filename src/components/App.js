e766f9d4eaeb76cfddcaab939b457768416983c3
USER_NAME <147196596+tanishka786@users.noreply.github.com>
2025-07-08 00:15:46 +0530

    editor setup done

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,42 @@
-import react from "react";
-import Editor from "./Editor";
+import React, { useState } from 'react';
+import Editor from './Editor'
 function App() {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+
+
+
   return (
     <>
-      <div classname="pane top-pane">
-        <Editor/>
-        <Editor/>
-        <Editor/>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+         <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+         <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
       </div> {/*upper part of coding i.e html ,css and js window SO 3 EDITOR are mentioned */}
-      
-      
-      <div classname="pane"> {/*output part */}
-        <iframe 
-             title="output"
-             sandbox="allow-scripts"
-             frameBorder ="0"
-             width="100%"
-             height="100%"
-        >
-        </iframe> 
-        {/* for writing text */}
+      <div className="pane"> {/*output part */}
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          style={{ border: 'none' }}
+          width="100%"
+          height="100%"
+        />{/* for writing text */}
       </div>
     </>
   )
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,15 +2,40 @@ import React from 'react'
 import 'codemirror/lib/codemirror.css'  //importing the above line as it contains all the css required for code editor layout
 import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml' // xml is same as html so they both gonea work as itis.
-import 'codemirror/mode/css/css'
 import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
 
-export default function Editor() {
+export default function Editor(props) {
+    const {
+        language,
+        displayName,
+        value,
+        onChange
+    } = props
+
+    function handleChange(editor, data, value) {
+        onChange(value)
+    }
+
     return (
-        <div>
-            EDITOR
+        <div clssName="editor-container">
+            <div className="editor-title">
+                {displayName}
+                <button>O/C</button>
+            </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className="code-mirror-wrapper"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    theme: "material",
+                    lineNumbers: true
+                }}
+            />
         </div>
     )
 }
-
-
